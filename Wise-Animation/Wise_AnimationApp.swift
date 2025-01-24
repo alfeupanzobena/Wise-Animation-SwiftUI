6960a93c2da89fbfdc1669652915e9bead411249
alfeupanzobena <alfeupanzobena28@gmail.com>
2025-01-24 05:23:27 +0100

    Initial Commit

---
+++ Wise-Animation/Wise_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wise_AnimationApp.swift
+//  Wise-Animation
+//
+//  Created by Alfeu Panzo Bena on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Wise_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
